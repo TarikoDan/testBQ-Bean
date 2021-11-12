3108cca77ce471fe31108267cf5a5136a4980ec4
Taras_Danylyshyn <Taras_Danylyshyn@epam.com>
2021-11-12 18:12:10 +0200

    write csv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,7 +15,7 @@ import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static <T> void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         PipelineOptions options = PipelineOptionsFactory.create();
         Pipeline p = Pipeline.create(options);
 
@@ -28,41 +28,48 @@ public class Main {
         Schema schema = new Schema.Parser().parse(new File(inputFileSchema));
         String header = schema.getFields().stream()
                 .map(field -> field.name()).collect(Collectors.joining(","));
-
-        PCollection<GenericRecord> records =
-                p.apply(AvroIO.readGenericRecords(schema)
+        PCollection<GenericRecord> records = p.apply("Read records from Avro file",
+                AvroIO.readGenericRecords(schema)
                         .from(inputFilePath));
 
+        //Transform collection to KV<>
         PCollection<KV<String, GenericRecord>> keyedByYear =
                 records
-                        .apply(WithKeys.of(new SimpleFunction<GenericRecord, String>() {
+                        .apply("Create a key for the year",
+                                WithKeys.of(new SimpleFunction<GenericRecord, String>() {
                             @Override
                             public String apply(GenericRecord s) {
                                 return s.get("year").toString();
                         }}));
 
+        // Calculating the most popular name for each year
+        // 1-st VAR : [GroupByKey -> Combine.groupedValues]
 //        PCollection<KV<String, Iterable<GenericRecord>>> grouped =
 //                keyedByYear.apply("group", GroupByKey.<String, GenericRecord>create());
 //        PCollection<KV<String, GenericRecord>> groupedValues = grouped.apply(Combine.groupedValues(
 //                Max.of(new GenComparator())
 //        ));
 
+        // 2-nd VAR : [Combine.perKey]
         PCollection<KV<String, GenericRecord>> combined =
-                keyedByYear.apply(Combine.perKey(
+                keyedByYear.apply("Calculating the most popular name for each year",
+                        Combine.perKey(
                         Max.of(new NumberComparator())));
 
-        PCollection<GenericRecord> mostPopularNames = combined.apply(Values.create());
+        PCollection<GenericRecord> mostPopularNames =
+                combined.apply("Extract values from KV<>", Values.create());
 //        print(mostPopularNames);
 
-        PCollection<String> lines = mostPopularNames.apply(ParDo.of(new ExtractValues()));
+        // Preparing records for writing to CSV
+        PCollection<String> lines =
+                mostPopularNames.apply("Extract field's names",
+                ParDo.of(new ExtractValues()));
 
-        lines.apply(
-                        "Write CSV formatted data",
+        lines.apply("Write CSV formatted data",
                         TextIO.write().to(outputFilePath)
                                 .withoutSharding()
                                 .withHeader(header)
                                 .withSuffix(".csv"));
-//
 
         p.run().waitUntilFinish();
 
@@ -73,11 +80,8 @@ public class Main {
                 .apply(
                 "Preview Result",
                 MapElements.into(TypeDescriptors.strings())
-                        .via(
-                                x -> {
-                                    System.out.println(x);
-                                    return "";
-                                }));
+                        .via(x -> {System.out.println(x);
+                                    return "";}));
     }
 
     private static class NumberComparator implements Comparator<GenericRecord>, Serializable {
@@ -94,6 +98,7 @@ public class Main {
         @ProcessElement
         public void processElement(ProcessContext c) {
             GenericRecord element = c.element();
+            assert element != null;
             String state = element.get("state").toString();
             String gender = element.get("gender").toString();
             String year = element.get("year").toString();
